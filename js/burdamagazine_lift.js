9c2f18321aebfc6e410c483996f7dca3d315115d
Christian Fritsch <chr.fritsch@gmx.net>
2016-05-09 14:37:30 +0200

    Change minor texts

--- js/burdamagazine_lift.js
+++ js/burdamagazine_lift.js
@@ -2,7 +2,6 @@ var lift_init = Drupal.acquia_lift.initialize.bind({});
 
 (function ($, Drupal, settings) {
 
-  console.log(settings);
   "use strict";
 
   Drupal.acquia_lift.initialize = function () {
@@ -11,7 +10,6 @@ var lift_init = Drupal.acquia_lift.initialize.bind({});
 
     if (!settings.enable_search_engine_filter || !ref.match(/^https?:\/\/(www\.)?google\.(([a-z]{2,3})|(com?\.[a-z]{2,3}))(\/|$)/i)) {
 
-      console.log('Dri');
       lift_init();
     }
   };
